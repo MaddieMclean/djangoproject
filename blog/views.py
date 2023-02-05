d3c12f313e7c569138c6d37a69a9deade35c6519
Maddie Mclean <madeline.joanna.mclean@gmail.com>
2023-02-05 16:53:05 +0000

    adding test pages

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+
+# Create your views here.
+
+def home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Blog Home</h1>")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>About</h1>")
